149a8bf6ec565978e2c78010018cec359800e157
Magnuuus92 <Magnus92.kodehode@gmail.com>
2025-09-22 14:59:40 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,41 @@
+using Microsoft.JSInterop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "HELLO WORLD");
+
+app.MapGet("/Ingenting", () =>
+{
+    return $"Ingenting ";
+});
+app.MapGet("/greetings", (string name) =>
+{
+    if (name == "Magnus")
+    {
+        return $"Hei MAGNU$";
+    }
+    else
+    {
+        return $"Greetings: {name}";
+
+    }
+});
+
+app.MapPost("/post", (string body) =>
+{
+    return $"Post; Path: / {body}";
+
+});
+
+
+app.MapPut("/", (string body) =>
+{
+    return $"Put; Path: / {body}";
+});
+
+app.MapDelete("/delete", (string body) =>
+{
+    return $"Delete; Path: / {body}";
+});
 
 app.Run();
